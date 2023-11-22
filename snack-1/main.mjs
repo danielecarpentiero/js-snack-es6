a83e1fc14b13ed96f28536b13f4fb5e4baf089d9
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-22 17:06:46 +0100

    feat: visualizzazione bici con peso inferiore

--- snack-1/main.mjs
+++ snack-1/main.mjs
@@ -2,53 +2,58 @@ const bikes = [
   {
     bikeName: "augusto",
     bikeWeight: 38,
-    bikeImage: "../img/81KvpYgtwpL._AC_UF894,1000_QL80_.jpg",
+    bikeImage: "img/81KvpYgtwpL._AC_UF894,1000_QL80_.jpg",
   },
   {
     bikeName: "giorgina",
     bikeWeight: 22,
-    bikeImage: "../img/1692-0-0-2687-10000-5876-1000.jpg",
+    bikeImage: "img/1692-0-0-2687-10000-5876-1000.jpg",
   },
   {
     bikeName: "teodora",
     bikeWeight: 26,
-    bikeImage: "../img/2023-Canyon-Ultimate-CFRCrop-9f7857a.jpg",
+    bikeImage: "img/2023-Canyon-Ultimate-CFRCrop-9f7857a.jpg",
   },
   {
     bikeName: "fausta",
     bikeWeight: 19,
-    bikeImage: "../img/best_lightweight_bikes_21.jpg",
+    bikeImage: "img/best_lightweight_bikes_21.jpg",
   },
   {
     bikeName: "alda",
     bikeWeight: 30,
-    bikeImage: "../img/Lightweight_Urgestalt_bikeTest_web_Main_side.jpg",
+    bikeImage: "img/Lightweight_Urgestalt_bikeTest_web_Main_side.jpg",
   },
   {
     bikeName: "duda",
     bikeWeight: 41,
-    bikeImage: "../img/MG_BR_HC-2021_Rebecca-Richardson_01-77d5685.jpg",
+    bikeImage: "img/MG_BR_HC-2021_Rebecca-Richardson_01-77d5685.jpg",
   },
   {
     bikeName: "manila",
     bikeWeight: 23,
-    bikeImage: "../img/wQSfPdUmv6RrZEecuN4eU-320-80.jpg",
+    bikeImage: "img/wQSfPdUmv6RrZEecuN4eU-320-80.jpg",
   },
 ];
 console.log(bikes[5].bikeWeight);
 const bikeContainer = document.getElementById("contenitore");
 
+let minWeightBike = bikes[0];
+
 bikes.forEach((bike) => {
-  bikeContainer.innerHTML = ` <div class="bike">
-  <div class="image">
-    <img
-      src="${bike.bikeImage}"
-      class="img-fluid"
-      alt=""
-    />
-    <h2 class="text-center">${bike.bikeName}</h2>
-    <h3 class="text-center">Peso: ${bike.bikeWeight}</h3>
-  </div>
-</div>`;
-  console.log(bike);
+  if (bike.bikeWeight < minWeightBike.bikeWeight) {
+    minWeightBike = bike;
+  }
 });
+
+const minWeightBikeHTML = `
+  <div class="bike">
+    <div class="image">
+      <img src="${minWeightBike.bikeImage}" class="img-fluid" alt="" />
+      <h2 class="text-center">${minWeightBike.bikeName}</h2>
+      <h3 class="text-center">Peso: ${minWeightBike.bikeWeight}</h3>
+    </div>
+  </div>
+`;
+
+bikeContainer.innerHTML = minWeightBikeHTML;
