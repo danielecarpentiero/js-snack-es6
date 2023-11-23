4662b1bc890d35490ba41481603c4c32e782d272
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-23 11:30:08 +0100

    feat: bonus snack 3

--- bonus-snack-3/main.mjs
+++ bonus-snack-3/main.mjs
@@ -1,1 +1,20 @@
 "use strict";
+
+function nuovoArray(arr, min, max) {
+  switch (true) {
+    case !Array.isArray(arr):
+    case min > max:
+    case min < 0 || max > arr.length - 1:
+      return error;
+    default:
+      return arr.filter((element, index) => index) >= min && index <= max;
+  }
+}
+
+const numeri = [1, 2, 3, 4, 5, 6, 7];
+
+let result = nuovoArray(numeri);
+
+/* let result = nuovoArray(numeri, 2, 6); */
+console.log(numeri);
+console.log(result);
